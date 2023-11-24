84cd0cf4b97d72fc5845f28afce5760be92125c6
vijaykumardumpeti <vijaydumpeti70@gmail.com>
2023-11-24 19:35:03 +0530

    backend done

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const initilizeServer = ()=>{
+    app.listen(3000, ()=>{
+        console.log('server started at http://localhost:3000')
+    })
+}
+
+initilizeServer()
+
+
+app.get('/v1/users', async (req, res)=>{
+    const usersResponse  = await axios.get('https://jsonplaceholder.typicode.com/users')
+    const users = usersResponse.data
+    
+    const postsResponse = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    const posts = postsResponse.data
+
+
+    let combinedData = users.map((user)=> ({
+        ...user,
+        posts: posts.filter(post=> post.userId === user.id)
+    }))
+
+    const searchText  = req.query.searchText
+    if(searchText){
+        combinedData = combinedData.filter((obj)=> obj.name.toLowerCase().includes(searchText.toLowerCase()))
+    }
+    res.send(combinedData)
+
+})
